0a61c2a53f35472f4cf9326b0f2a5e5cbfe587d2
LuizEnriqueSilva-Dev <silvasantosenriqueluiz@gmail.com>
2026-05-13 16:50:39 -0300

    Update server.js

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.get("/onibus", async (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log("Servidor rodando em http://localhost:3000");
+    console.log("Servidor rodando em https://moveasy-mobilidade-e-conforto.onrender.com/");
 });
 
 app.get('/healthz', (req, res) => {
